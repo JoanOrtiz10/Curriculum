beeb8568dc6890ac1ceb849f6ddba38a3cb68922
JoanOrtiz10 <ortizjoan404@gmail.com>
2023-05-10 17:05:49 -0500

    js

---
+++ JS/clase.js
@@ -0,0 +1,37 @@
+//Variables
+const nameuser = "joanortiz";
+var x = "valor";
+let y = "valor";
+
+let a = 20;
+let b = 5.5;
+let string = "my name is Joan";
+let number ="12154";
+let Boolean_uno = false;
+let Boolean_two =true;
+let array = [1,2,3,4,5,6]
+let array_string =["lunes","martes","miercoles"];
+//operadores aritmeticos : + - / * %
+let suma =1+5+a;
+let resta =1-5-a;
+let multi =1*5*a;
+let divi = 1/5/a;
+let modulo =10%2;
+let elev = 5^2;
+console.log(suma+"-"+resta+"-"+multi+"-"+divi+"-"+modulo+"-"+elev);
+//operadores de comparacion : = / == / === / <> / <= / >= / > / <
+// operadores logicos : And && /OR ||/
+
+if(10>5){
+    for(let i=0; i< array_string.length; i++){
+        console.log( array_string[i]);
+
+    }
+}
+let j=5;
+let k=10;
+
+while(k>=j){
+    console.log("Entra");
+    j++
+}
